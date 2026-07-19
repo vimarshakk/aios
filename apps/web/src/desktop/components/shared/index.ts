0fee6675be79129f6522dea661b8544bcc123f36
vimarshakk <vimarshakk@users.noreply.github.com>
2026-07-20 00:42:18 +0530

    feat(desktop): add index.ts

---
+++ apps/web/src/desktop/components/shared/index.ts
@@ -0,0 +1,6 @@
+export { GlassCard } from "./GlassCard";
+export { SectionHeader } from "./SectionHeader";
+export { MetricCard } from "./MetricCard";
+export { StatusBadge } from "./StatusBadge";
+export { TimelineItem } from "./TimelineItem";
+export { ProgressCard } from "./ProgressCard";
